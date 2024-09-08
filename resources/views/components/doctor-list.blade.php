1fb69a0cd10097201a426ef263613bbbafbb3d61
AndresElacion <andreselacioniii@gmail.com>
2024-09-08 14:39:14 +0800

    added route to doctor on dashboard

--- resources/views/components/doctor-list.blade.php
+++ resources/views/components/doctor-list.blade.php
@@ -4,11 +4,11 @@
           <div>
             <h2 class="font-semibold text-slate-800 uppercase">Doctor: <span class="text-slate-500 text-xl">{{ $totalDoctors }}</span></h2>
           </div>
-            <div class="hover:rotate-180 hover:scale-125 transition-transform">
+            <a href="{{ route('doctor.index')}}" class="hover:rotate-180 hover:scale-125 transition-transform">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
                 </svg>
-            </div>
+            </a>
         </header>
 
         <div class="rounded-lg border border-gray-200">
@@ -40,9 +40,6 @@
                     </td>
                   </tr>
                 @endforeach
-                
-  
-                
               </tbody>
             </table>
           </div>
