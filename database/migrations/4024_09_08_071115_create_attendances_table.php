7a1f68de0184db83864aa80eb352a73971409947
AndresElacion <andreselacioniii@gmail.com>
2024-09-09 17:27:41 +0800

    added full functionality for the attendance(login,break,break,lunch)

--- database/migrations/4024_09_08_071115_create_attendances_table.php
+++ database/migrations/4024_09_08_071115_create_attendances_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('attendances')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('status');
             $table->boolean('overbreak')->default(false);
             $table->timestamps();
