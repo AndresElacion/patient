73127ebcad40e2db3ecd1cc3ca780eb7ee6f7a35
AndresElacion <andreselacioniii@gmail.com>
2024-09-12 09:38:18 +0800

    added staff query and profile

--- database/migrations/0000_09_07_040511_create_roles_table.php
+++ database/migrations/0000_09_07_040511_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name'); // 1 admin, 2 doctor, 3 patient, 4 staff
             $table->timestamps();
         });
     }
