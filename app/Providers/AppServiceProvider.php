a047ba387876eb29b3384d10ef753235113b3d54
AndresElacion <andreselacioniii@gmail.com>
2024-10-02 18:50:14 +0800

    fixed bugs ready for deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,30 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+        Model::unguard();
+        Schema::defaultStringLength(191);
     }
 }
--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run Laravel migrations
-php artisan migrate:fresh --force --seed
-
-# Serve the application
-php artisan serve --host=0.0.0.0 --port=8000
